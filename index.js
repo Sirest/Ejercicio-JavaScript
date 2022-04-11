e35e37fc197f4382ee40302888997c819180d15b
SIRA <siranaadamyan@gmail.com>
2022-04-11 17:29:54 +0200

    he hecho el primer ejercicio de Java

---
+++ index.js
@@ -0,0 +1,127 @@
+// alert("ey");
+console.log("Hola Mundo")
+let variableSinValor
+let booleano1= true;
+let booleano2= false;
+const PI =  3.14;
+const TAU = 2 * PI;
+const miNombre = "Sira";
+const miNumeroFav = 7;
+let booleano = true;
+console.log(miNombre);
+console.log(miNumeroFav);
+console.log( "Mi nombre es Euralio y mi numero favorito es"+ 7);
+console.log("SERÉ UN CRACK EN JavaScript AL TERMINAR EL BOOTCAMP");
+const crack = "Hola soy un crack";
+console.log(crack.slice(0, 5))
+let index=String(miNumeroFav);
+console.log(index);
+const expresión = "expresión";
+console.log("Hola soy un ${expresión} crack");
+console.log(2);
+
+// ARRAYS
+const arrayVacio =[];
+const arrayNumeros =[0,1,2,3,4,5,6,7,8,9];
+const arrayNumeros2 =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+const arrayNumerosPares =[0,2,4,6,8];
+const arrayNumerosNeg =[-0,-1,-2,-3,-4,-5,-6,-7,-8,-9,];
+const arrayNumero =["Hola","Mundo"];
+const loGuardoTodo =["hola","que", 23,42.33,"tal"];
+const arrayDeArrays =[756,"nombre",225,"apellido",298,"direccion"];
+console.log(loGuardoTodo.length);
+const loguardotodo = "elemento";
+loGuardoTodo.push(loguardotodo);
+console.log(loGuardoTodo);
+//  const loGuardoTodo =["Eurelio"];
+//  console.log=(loGuardoTodo);
+
+//  objetos
+
+const Coche = {
+    marca: "Volvo",
+    modelo: "xc60",
+    matricula: "234567",
+}
+const Casa ={
+    coodPostal: "2345",
+    calle: "sardenya",
+    portal: "123",
+    piso: "34",
+}
+const FullStackDeveloper={
+    lenguajes: ["javascript"],
+    proyectos: ["mi pagina personal"] 
+}
+const perro={
+    nombre:"piso",
+    raza:"idbul",
+    color:"marron",
+    edad:"3",
+}
+const noticia={
+    titular: "sobremiperro",
+    cuerpo: "2",   
+}
+ const Persona={
+     nombre:"Hamlet",
+     apellido:"Gaspari",
+     edad: "20",
+ }
+ console.log(Persona.nombre);
+ console.log(FullStackDeveloper);
+
+ const portatil={
+     marca: "MacBook"
+ }
+ console.log(portatil.marca);
+ console.log("portatil","marca");
+ const concierto={
+     grupos: ["mecano",],
+ }
+ console.log(concierto.grupos);
+  const led={
+      color: ["rojo","verde","azul"],
+  }
+  const RGB = ["rojo","verde","azul"];
+  console.log(RGB);
+   const O_Error={
+    código: "5466"
+   }
+   console.log(O_Error.código);
+
+   const Grupo={
+    integrantes: ["cantante","gitarra","azul"],
+}
+console.log(Grupo.integrantes);
+ 
+const Impresora={
+ tinta: ["azul","verde","rojo"]
+}
+console.log(Impresora.tinta);
+
+const Movil={
+    especificaciones:["grande","blanco","samsung"],
+}
+console.log(Movil.especificaciones);
+
+const Portatil={
+    marca:"MSI",
+}
+console.log(Portatil.marca);
+
+const Concierto={
+    grupos:"Guns N",
+}
+console.log(Concierto.grupos);
+
+const Fecha={
+    Fechadehoy:["27","de", "marzo"],
+}
+console.log(Fecha.Fechadehoy);
+
+const Grupos={
+integrantes: ["cantante","gitarra"],
+}
+
+console.log(Grupos.integrantes);
